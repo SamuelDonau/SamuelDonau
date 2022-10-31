80370e8e36755a542cfad93682d7329774140e96
Racco <samuel.donau@coderclass.nl>
2022-10-31 11:39:33 +0100

    popup

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function wip() {
   alert("Deze pagina is nog niet beschikbaar.");
 }
+
+function openInNewTab(url) {
+  window.open(url, '_blank').focus();
+}
+
+function nw() {
+  window.open("Belangrijke Brief.pdf", "_blank", "width=400", "height=400");
+}
